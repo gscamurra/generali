6f83f1b66b9d53ac01abf6832280bbc1682b9b00
gscamurra <giovanni.scamurra.adobe@gmail.com>
2025-07-30 02:15:05 +0200

    Update skills.js

--- blocks/skills/skills.js
+++ blocks/skills/skills.js
@@ -1,6 +1,7 @@
 export default function decorate(block, { listSkills }) {
-  let text = listSkills;
+  let text = listSkills[0];
 
+  console.log (listSkills[0]);
 
   block.innerHTML = `<ul>${text}</ul>`;
 }
